20562fe36ea27854083f631fc43d30a959e2073d
Davide2805 <davide.urbinati2@studio.unibo.it>
2022-05-17 23:18:14 +0200

    Commit homepage

--- animalhouse/src/App.js
+++ animalhouse/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
+import Card from './components/card';
+import ImmagineGame from './images/game.jpg';
+import ImmagineLogin from './images/immagine.jpg';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>Max Callegari</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state={
+    Cards:[
+      {
+        id:0,
+        immagine:ImmagineGame,
+        tipo:"Game",
+        descrizione:"Metti alla prova le tue capacità con tanti giochi a tema animale!",
+        bottone:"Gioco"
+      },
+      {
+        id:1,
+        immagine:ImmagineLogin,
+        tipo:"Login",
+        descrizione:"Registrati per ottenere tante altre funzionalità!",
+        bottone:"Login"
+      }
+    ]
+  }
+  render(){
+    return (
+    <>
+    <Navbar/>
+    <div className='container'>
+      <h1>La pagina preferita dagli amanti degli animali!</h1>
+      <div className='row'>
+        {
+          this.state.Cards.map(card=>(
+            <Card
+            key={card.id}
+            immagine={card.immagine}
+            tipo={card.tipo}
+            descrizione={card.descrizione}
+            tipoBottone={card.bottone}
+            />
+          ))
+        }
+      </div>
     </div>
+    </>
   );
+  }
 }
 
 export default App;
---
+++ animalhouse/src/components/navbar.js
@@ -0,0 +1,10 @@
+function Navbar(){
+    return(
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#page-top">Animal House</a>
+      </div>
+    </nav>
+    );
+}
+export default Navbar;
